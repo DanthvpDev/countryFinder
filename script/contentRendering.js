fb6c5b1503239998449f58e718278ab25b1f3581
Daniel Hernández <danthdev02@gmail.com>
2024-12-03 01:33:40 -0600

    Search by country name finished, index layout finished, search by region started

---
+++ script/contentRendering.js
@@ -0,0 +1,25 @@
+let gridContainer = document.getElementById('gridContainer');
+
+function ShowCountryCards(data) {
+    if(gridContainer.children.length > 0) {
+        console.log(data)
+        Array.from(gridContainer.children).forEach(element => {
+            element.remove();
+        });
+    }
+    data.forEach(element => {
+        let newDiv = document.createElement('div');
+        newDiv.className += 'col-3 bg-light text-black text-start py-3 px-3 rounded-2 cardShadow';
+        newDiv.innerHTML = `
+                <div class="col-12 d-flex justify-content-between w-100">
+                    <h3 class="py-1 text-primary">${typeof(element.name) === 'object' ? element.name.common : element.name}</h3>
+                    <img src='${typeof(element.flags) === 'object' ? element.flags.png : element.flag}' class='object-fit-contain' width="50" height="40"></img>
+                </div>
+                <p class="text-dark fs-5 cl-12 pb-2 w-100">${element.region}</p>
+                <a class="btn btn-light fw-semibold btnCountryInfo">More</a>
+        `
+        gridContainer.appendChild(newDiv);
+    });
+    return;
+    
+}
--- script/main.js
+++ script/main.js
@@ -1,30 +1,51 @@
 const searchBtn = document.getElementById('searchBtn');
-const input = document.getElementById('inputText');
+const inputText = document.getElementById('inputText');
+const form = document.getElementById('formSearch');
 
+//? Filter among data that includes the input text
 function SearchCountryByName(data, prompt) {
     return data.filter(e => e.name.common.toLowerCase().includes(prompt));
 }
 
 //? This initializes the search events 
 function InitializeSearchEvents(data) {
-        searchBtn.addEventListener('click',  ()=> {
-            let inputData = input.value.toLowerCase();
-            SearchCountryByName(data, inputData);
-        }, false);
+
+    //* Search button event initialization
+    searchBtn.addEventListener('click', () => {
+        let inputData = inputText.value.trim().toLowerCase();
+        let filteredData = SearchCountryByName(data, inputData);
+        if (filteredData.length > 0) ShowCountryCards(filteredData)
+        else console.log('NO');
+    }, false);
+
+    //* Search whent enter event initialization
+    inputText.addEventListener('keypress', e => {
+        if (e.key == 'Enter') {
+            e.preventDefault();
+            let inputData = inputText.value.trim().toLowerCase()
+            let filteredData = SearchCountryByName(data, inputData);
+            if (filteredData.length > 0) ShowCountryCards(filteredData)
+        }
+    })
 }
 
 //* Function builds up the functionality
 async function StartApp() {
-    try { 
+    try {
         //* Checks if API's info is saved in local storage 
         let storageData = await SavingLocalStorage();
-        
+        let dataForCards = storageData.map(element => ({
+            name: element.name.common,
+            region: element.region,
+            flag: element.flags.png
+        }));
         InitializeSearchEvents(storageData);
+        ShowCountryCards(dataForCards);
     }
-    catch(exc) {
+    catch (exc) {
         console.log(`Hubo un error al iniciar la aplicación ${exc}`)
     }
 }
 
 
-document.addEventListener('DOMContentLoaded', StartApp,false);
+document.addEventListener('DOMContentLoaded', StartApp, false);
