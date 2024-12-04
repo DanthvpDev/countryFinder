2ae145d8a17a95d43f5a743941b54e532e18fa37
Daniel Hernández <danthdev02@gmail.com>
2024-12-04 09:14:44 -0600

    Map hover, search finished

--- script/searching.js
+++ script/searching.js
@@ -1,9 +1,14 @@
-//? Filter among names of countries that includes the input text
+//? Filter among names of countries that includes the prompt
 function SearchCountryByName(data, prompt) {
     return data.filter(e => e.name.common ? e.name.common.toLowerCase().includes(prompt) : e.name.toLowerCase().includes(prompt));
 }
 
-//? Filter among regions that includes the input text
+//? Filter among regions that includes the prompt
 function SearchCountryByRegion(data, prompt) {
     return data.filter(e => e.region.toLowerCase().includes(prompt));
+}
+
+//? Filter among continents that includes the prompt
+function SearchCountryByContinent(data, prompt) {
+    return data.filter(e => e.continents[0].toLowerCase().includes(prompt));
 }
---
+++ script/worldMap.js
@@ -0,0 +1,40 @@
+const continentsSVG = document.querySelectorAll('.continents')
+const p = document.getElementById('continentName');
+
+
+Array.from(continentsSVG).forEach(continent => {
+    let id;
+
+    continent.addEventListener('click', (e)=> {
+        let region =  SearchCountryByContinent(JSON.parse(localStorage.getItem('countriesData')), id);
+        ShowCountryCards(region);
+     })
+
+    //? When over a contient, fills it  blue
+    continent.addEventListener('mouseover', (event)=> {
+        id = continent.getAttribute('id').split('_').join(' ');
+
+        [...continent.children].forEach(element => {
+            element.style.fill = 'rgb(44, 108, 187)'
+        });
+
+        let mousePositionX = event.clientX;
+        let mousePositionY = event.clientY;
+        p.style.display = 'inline-block';
+        p.style.top = `${mousePositionY+120}px`
+        p.style.left = `${mousePositionX - 20}px`
+        p.textContent = id;
+        
+    })
+
+    //? When leave a contient, return the color to grey
+    continent.addEventListener('mouseleave', ()=> {
+        [...continent.children].forEach(element => {
+            element.style.fill = '#0000005f'
+        });
+
+        p.style.display = 'none';
+    })
+
+    
+});
